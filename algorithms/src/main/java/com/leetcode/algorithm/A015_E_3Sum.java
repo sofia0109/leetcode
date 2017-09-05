6dc12c537feec561bfd2b14c7b76e8659ece1c07
sofia <jianmei.yu0225@gmail.com>
2017-09-05 23:32:31 +0800

    3sum

--- algorithms/src/main/java/com/leetcode/algorithm/A015_E_3Sum.java
+++ algorithms/src/main/java/com/leetcode/algorithm/A015_E_3Sum.java
@@ -1,15 +1,53 @@
 package com.leetcode.algorithm;
 
-/**
- *
- *
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * <p>
  * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
- *
- *
+ * <p>
+ * <p>
  * Created by yujianmei on 2017/09/01.
  */
 public class A015_E_3Sum {
-    
+    /**
+     * Notice:
+     *
+     * The most complex part is : How to avoid duplicate array?
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0, end = nums.length - 3; i <= end; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int low = i + 1, high = nums.length - 1;
+                while (low < high) {
+                    int total = nums[low] + nums[high] + nums[i];
+                    if (total == 0) {
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low] == nums[low + 1]) low++;
+                        while (low < high && nums[high] == nums[high - 1]) high--;
+                        low++;
+                        high--;
+                    } else if (total > 0) {
+                        high--;
+                    } else {
+                        low++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, -1, 0, 1, 2};
+        new A015_E_3Sum().threeSum(nums);
+    }
 }
--- algorithms/src/main/java/com/leetcode/algorithm/A017_E_LetterComOfPhoneNum.java
+++ algorithms/src/main/java/com/leetcode/algorithm/A017_E_LetterComOfPhoneNum.java
@@ -1,15 +1,38 @@
 package com.leetcode.algorithm;
 
-/**
- *
- *
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+import java.util.ArrayList;
+import java.util.List;
 
- * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
- *
- *
+/**
+ * Given a digit string, return all possible letter combinations that the number could represent.
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.
+ * <p>
+ * <p>
  * Created by yujianmei on 2017/09/01.
  */
 public class A017_E_LetterComOfPhoneNum {
-    
+
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return null;
+        }
+        String[] tables = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> result = new ArrayList<>();
+        letterCombinations(result, digits, "", 0, tables);
+        return result;
+    }
+
+    private void letterCombinations(List<String> list, String digits, String cur, int depth, String[] tables) {
+        if (depth == digits.length()) {
+            list.add(cur);
+            return;
+        }
+
+        int index = digits.charAt(depth) - '0';
+        String table = tables[index];
+        for (int i = 0, len = table.length(); i < len; i++) {
+            String append = cur + table.charAt(i);
+            letterCombinations(list, digits, append, depth + 1, tables);
+        }
+    }
 }
