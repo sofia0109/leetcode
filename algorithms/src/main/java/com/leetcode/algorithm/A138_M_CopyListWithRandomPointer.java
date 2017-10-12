c0abcf8f1385ed05df5f89e40837d5014d1a505f
sofia <jianmei.yu0225@gmail.com>
2017-10-12 23:17:59 +0800

    solove problem 138 with another solution

--- algorithms/src/main/java/com/leetcode/algorithm/A138_M_CopyListWithRandomPointer.java
+++ algorithms/src/main/java/com/leetcode/algorithm/A138_M_CopyListWithRandomPointer.java
@@ -43,6 +43,47 @@ public class A138_M_CopyListWithRandomPointer {
         return fake.next;
     }
 
+    public RandomListNode copyRandomList2(RandomListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        //insert a temp node at every node.
+        RandomListNode iter = head, next;
+        while (iter != null) {
+            next = iter.next;
+            RandomListNode copy = new RandomListNode(iter.label);
+            iter.next = copy;
+            copy.next = next;
+            iter = next;
+        }
+
+        //copy random pointer
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        //split the original list to two lists
+        iter = head;
+        RandomListNode newHead = null;
+        while (iter != null) {
+            RandomListNode copyOf = iter.next;
+            if (newHead == null) {
+                newHead = copyOf;
+            }
+            iter.next = iter.next.next;
+            iter = copyOf.next;
+            if (iter != null) {
+                copyOf.next = iter.next;
+            }
+        }
+        return newHead;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
